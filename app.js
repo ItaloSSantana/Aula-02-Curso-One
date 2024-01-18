fc0d016025e681cf06d360cc7beb1e31e402560b
Italo Santana <italo_santana13@hotmail.com>
2024-01-18 14:24:22 -0300

    adiciona function para textos

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do Número Secreto';
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Jogo do Número Secreto';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número de 1 a 10';
+// let paragrafo = document.querySelector('p');
+// paragrafo.innerHTML = 'Escolha um número de 1 a 10';
+
+exibirTextoNaTela('h1','Jogo do Número Secreto');
+exibirTextoNaTela('p','Escolha um número de 1 a 10');
+
+function exibirTextoNaTela (tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
 
 function verificarChute() {
-    console.log("Verificado")
+    console.log("Verificado");
 }
