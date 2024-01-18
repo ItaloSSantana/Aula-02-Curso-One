6752295d9739d13557030e60b512aa981661fe54
Italo Santana <italo_santana13@hotmail.com>
2024-01-18 15:40:38 -0300

    gera numero aleatorio

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-// let titulo = document.querySelector('h1');
-// titulo.innerHTML = 'Jogo do Número Secreto';
-
-// let paragrafo = document.querySelector('p');
-// paragrafo.innerHTML = 'Escolha um número de 1 a 10';
-
-exibirTextoNaTela('h1','Jogo do Número Secreto');
-exibirTextoNaTela('p','Escolha um número de 1 a 10');
+let numeroSecreto = gerarNumeroAleatorio(10);
 
 function exibirTextoNaTela (tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
+exibirTextoNaTela('h1','Jogo do Número Secreto');
+exibirTextoNaTela('p','Escolha um número de 1 a 10');
+
 function verificarChute() {
-    console.log("Verificado");
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
+    if (chute == numeroSecreto) {
+     console.log('acertou');
+     } else {
+        console.log('tente novamente');
+     }
+    
 }
+
+function gerarNumeroAleatorio(cap) {
+    return parseInt(Math.random() * cap + 1)
+}
+
